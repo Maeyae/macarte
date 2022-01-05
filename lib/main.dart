05e77f73eb991eccc30c4d19bb5949ac0457bd4f
maeva <maeganelys@gmail.com>
2022-01-05 22:25:19 +0400

    defi250%

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.green,
+              height: 100,
+              width: 100,
+              child: Text("Ohayo1"),
+            ),
+            Container(
+              color: Colors.red,
+              width: 100.0,
+              child: Text("Ohayo2"),
+            ),
+            Container(
+              color: Colors.blue,
+             width: 100.0,
+              child: Text("Ohayo3"),
+            ),
+            Container(
+              color: Colors.yellow,
+              height: 100,
+              width: 100,
+              child: Text("Ohayo4"),
+            ),
+          ],
+          ),
+        )
       ),
-    ),
   );
+}
 }
